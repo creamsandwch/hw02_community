dffd0cc6d279f844db201c0e7b400cbb8d962980
creamsandwch <neuronrus1996@gmail.com>
2022-11-03 11:41:56 +0500

    added paginator to group_list and index, modified posts/views accordingly

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.conf import settings
+from django.core.paginator import Paginator
 
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.select_related(
+    post_list = Post.objects.select_related(
         'group',
         'author'
-    )[:settings.POSTS_VIEWED]
+    )
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:settings.POSTS_VIEWED]
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
